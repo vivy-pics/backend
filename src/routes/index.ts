298ae08ad5ac8518e18d3273dba880f7930091d6
Lewis Crichton <lewi@lewisakura.moe>
2022-11-15 18:12:30 +0000

    add version route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,20 @@
+import fs from "node:fs";
+
 import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import type { FastifyPluginAsync } from "fastify";
+
+const version = (() => {
+	// eslint-disable-next-line unicorn/prevent-abbreviations
+	const ref = fs.readFileSync(".git/HEAD", "utf8").trim();
+	if (!ref.includes(":")) {
+		return ref.slice(0, 7);
+	}
+
+	return fs
+		.readFileSync(`.git/${ref.slice(5)}`, "utf8")
+		.trim()
+		.slice(0, 7);
+})();
 
 const plugin: FastifyPluginAsync = async (fastify, _options) => {
 	const f = fastify.withTypeProvider<TypeBoxTypeProvider>();
@@ -11,6 +26,8 @@ const plugin: FastifyPluginAsync = async (fastify, _options) => {
 		pong: true,
 		vivy: "What does it mean to put your heart into something?",
 	}));
+
+	f.get("/version", async () => ({ version }));
 };
 
 export default plugin;
